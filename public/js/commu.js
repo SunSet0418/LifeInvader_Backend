4431596d021ecdc40b044ee8a164bf31fbe74ba1
Jhun24 <jjanghuni102@naver.com>
2017-11-17 23:14:11 +0900

    yeah

--- public/js/commu.js
+++ public/js/commu.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
         method:"POST",
         url:"/post/list",
         success:function (data) {
+            console.log();
             var inputData = "";
-            for(var i = data.length - 1; i>-1; i--){
+            console.log(data["data"][0]);
+            for(var i = 0; i<data["data"].length; i++){
                 inputData += '<div class="topic-content">';
-                inputData += '<h4>토픽 : '+data[i]["topic"]+'</h4>';
-                inputData += '<p>'+data[i]["title"]+'</p></div>';
+                inputData += '<h4>토픽 : '+data["data"][i]["topic"]+'</h4>';
+                inputData += '<p>'+data["data"][i]["title"]+'</p></div>';
             }
 
             $(".recent-topic").append(inputData);
